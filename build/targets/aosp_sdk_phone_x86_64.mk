be268f191ba82dd49918db0527fdd2953c6225b1
Simão Gomes Viana <devel@superboring.dev>
2022-10-03 01:16:03 +0200

    Add workaround for data dir issue

--- build/targets/aosp_sdk_phone_x86_64.mk
+++ build/targets/aosp_sdk_phone_x86_64.mk
@@ -21,3 +21,8 @@ PRODUCT_NAME := aosp_sdk_phone_x86_64
 PRODUCT_DEVICE := emulator_x86_64
 PRODUCT_MODEL := $(CUSTOM_PRODUCT_NAME) for x86_64
 PRODUCT_BRAND := Generic
+
+# Workaround "data" dir issue (https://stackoverflow.com/questions/73918125/how-to-fix-the-sdk-addon-data-copy-build-error)
+LOCAL_DATA_OUT_DIR := $(TOP)/out/target/product/$(PRODUCT_DEVICE)/data
+$(shell mkdir -p $(LOCAL_DATA_OUT_DIR))
+$(shell echo "Workaround of data dir issue active, created $(LOCAL_DATA_OUT_DIR)" >&2)
