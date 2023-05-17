615c169f183e0330d6502babceac987ea209388f
Nolen Johnson <johnsonnolen@gmail.com>
2023-05-17 13:28:36 -0400

    soong: Allow Permissive SELinux by make flag
    * Most of us make dumb bringup commits, instead just
      set this as an env var.
    
    Change-Id: I684d7d809605fdf912cbc00f804427412307f180
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -50,7 +50,8 @@ SOONG_CONFIG_customGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    with_selinux_permissive
 
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
 SOONG_CONFIG_customGlobalVars += \
@@ -84,6 +85,7 @@ SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK
 SOONG_CONFIG_customQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_customGlobalVars_with_selinux_permissive := $(WITH_SELINUX_PERMISSIVE)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
