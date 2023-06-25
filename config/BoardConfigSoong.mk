6d3e9089ddbec7e9a31f7d15c3650d36fa6b5bcb
Noah Anleitner <noah.anleitner@halogenos.org>
2023-06-25 09:12:03 +0200

    rename Config

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -75,7 +75,7 @@ TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
 TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
 
 # Soong value variables
-SOONG_CONFIG_evolutionGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
+SOONG_CONFIG_customGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_customGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_customGlobalVars_disable_bluetooth_le_read_buffer_size_v2 := $(TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2)
 SOONG_CONFIG_customGlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_DISABLE_BLUETOOTH_LE_SET_HOST_FEATURE)
