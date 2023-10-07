4a040d7ad46b284f4052eac9a02ef4ae45af2caa
Simão Gomes Viana <devel@superboring.dev>
2023-10-07 17:14:47 +0200

    Add optional announcement

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,6 @@
 
+ifeq ($(CUSTOM_PRODUCT_ANNOUNCE),true)
+$(shell echo "$(CUSTOM_PRODUCT) product activated, dir: $(CUSTOM_PRODUCT_DIR)" >&2)
+endif
+
 include $(CUSTOM_PRODUCT_DIR)/config/branding.mk
