be00e1682fd3795aecfb9fd395a6d1364517249c
AdarshGrewal <adarshgrewal@gmail.com>
2024-06-19 15:46:50 +0530

    fixup! Hookup GRALLOC_HANDLE_HAS_UBWCP_FORMAT
    Change-Id: I44a9d7ce22f78862d73c96cf11fde856464952a9
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,7 +37,7 @@ SOONG_CONFIG_customGlobalVars += \
     camera_override_format_from_reserved \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
-    sdmcore_has_is_display_hw_available_func \
+    gralloc_handle_has_ubwcp_format \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
