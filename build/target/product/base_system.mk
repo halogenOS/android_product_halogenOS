58391aac88f89926d01d25944541b7d2b27ff212
Simão Gomes Viana <devel@superboring.dev>
2024-09-09 19:52:03 +0000

    Move properties to base_system.mk

---
+++ build/target/product/base_system.mk
@@ -0,0 +1,12 @@
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.custom.version=$(CUSTOM_VERSION) \
+    ro.custom.build.version=$(CUSTOM_PLATFORM_VERSION) \
+    ro.custom.display.version=$(CUSTOM_DISPLAY_VERSION) \
+    ro.custom.build_date=$(CUSTOM_BUILD_DATE) \
+    ro.custom.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    ro.custom.build_type=$(CUSTOM_BUILD_TYPE) \
+	ro.custom.device=$(CUSTOM_DEVICE) \
+	ro.custom.build.version.sp=$(PLATFORM_SECURITY_PATCH) \
+	ro.custom.build.device.maintainer=$(DEVICE_MAINTAINER) \
--- config/additional_props.mk
+++
@@ -1,5 +0,0 @@
-
-ADDITIONAL_SYSTEM_PROPERTIES += \
-	ro.custom.device=$(CUSTOM_DEVICE)
-	ro.custom.build.version.sp=$(PLATFORM_SECURITY_PATCH)
-	ro.custom.build.device.maintainer=$(DEVICE_MAINTAINER)
