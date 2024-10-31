4b4a49ed10a1d13609474752fef3915af91b1aa6
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-10-31 20:23:58 +0100

    Migrate to component-override for force enabled/disabled components
    Test:
    pm dump-package com.android.vending | grep disabledComponents -A20
    pm dump-package com.google.android.gms | grep disabledComponents -A150
    pm dump-package com.google.android.setupwizard | grep disabledComponents -A5
    
    Change-Id: Iddac5f0edc9a312df8d7c436312bad111289c1b4
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- config/common.mk
+++ config/common.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Component overrides
+PRODUCT_PACKAGES += \
+    custom-component-overrides.xml
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
