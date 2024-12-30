d7c88b047e19e5d9da237159406571189212371d
Simão Gomes Viana <devel@superboring.dev>
2024-12-30 21:51:37 +0100

    Remove backuptool
    We expect users to just flash what they need after flashing the ROM.

--- config/common.mk
+++ config/common.mk
@@ -28,22 +28,7 @@ endif
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.device_config.configuration.disable_rescue_party=true
 
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    $(CUSTOM_PRODUCT_DIR)/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    $(CUSTOM_PRODUCT_DIR)/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
-
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
-PRODUCT_COPY_FILES += \
-    $(CUSTOM_PRODUCT_DIR)/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
-    $(CUSTOM_PRODUCT_DIR)/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
-    $(CUSTOM_PRODUCT_DIR)/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/backuptool_ab.sh \
-    system/bin/backuptool_ab.functions \
-    system/bin/backuptool_postinstall.sh
-
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
