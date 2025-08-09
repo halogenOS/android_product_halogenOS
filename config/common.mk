476fd52cb37ca4f35a5b559107ec57989707c6ed
Simão Gomes Viana <devel@superboring.dev>
2025-08-10 00:15:59 +0200

    config: common: set default pihooks props

--- config/common.mk
+++ config/common.mk
@@ -196,6 +196,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.wm.debug.predictive_back_sysui_enab
 # Disable Storage Manger by default
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.storage_manager.enabled=false
 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.sussybox.fail=0 \
+    persist.sys.pihooks.disable=0
+
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(CUSTOM_PRODUCT_DIR)/build/target/product/security/custom
 
