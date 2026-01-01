00fad48cb524a60c29d0c184c76ea9f0e0f4bbc9
Simão Gomes Viana <devel@superboring.dev>
2026-01-02 00:55:42 +0100

    kernel: Set BOARD_PREBUILT_DTBOIMAGE at task time
    Setting BOARD_PREBUILT_DTBOIMAGE in BoardConfigKernel.mk causes
    Android 16's fsgen to panic when using out-of-tree $OUT, as it
    tries to verify the path exists in the source tree.
    
    Move the variable assignment to kernel.mk (task time) so fsgen
    doesn't see it during Soong analysis, but make still has it for
    the actual build rules.

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -228,11 +228,11 @@ else
     TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$(TARGET_KERNEL_CLANG_PATH)/bin:$(BUILD_TOP)/prebuilts/rust/$(HOST_PREBUILT_TAG)/$(TARGET_KERNEL_RUST_VERSION)/bin:$(BUILD_TOP)/prebuilts/clang-tools/$(HOST_PREBUILT_TAG)/bin:$$PATH
 endif
 
-# Set DTBO image locations so the build system knows to build them
+# Set DTBO defaults - actual path is set in kernel.mk at build time
+# (setting BOARD_PREBUILT_DTBOIMAGE here causes fsgen panic with out-of-tree $OUT)
 ifeq (true,$(filter true, $(TARGET_NEEDS_DTBOIMAGE) $(BOARD_KERNEL_SEPARATED_DTBO)))
     TARGET_KERNEL_DTBO_PREFIX ?=
     TARGET_KERNEL_DTBO ?= dtbo.img
-    BOARD_PREBUILT_DTBOIMAGE ?= $(TARGET_OUT_INTERMEDIATES)/DTBO_OBJ/arch/$(KERNEL_ARCH)/boot/$(TARGET_KERNEL_DTBO_PREFIX)$(TARGET_KERNEL_DTBO)
 endif
 
 # Set the default dtb target
